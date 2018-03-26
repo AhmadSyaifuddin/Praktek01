126dd3b728dcef60ab9080a33d76a1fd87aefca2
AhmadSyaifuddin <syaifuddinahmad598@gmail.com>
2018-03-26 17:23:49 +0800

    Membuat atribut dan behavior

--- src/praktek01/Sepatu.java
+++ src/praktek01/Sepatu.java
@@ -10,5 +10,17 @@ package praktek01;
  * @author udin'
  */
 public class Sepatu {
-    
+   double Ukuran;
+   String Merk;
+   String Warna;
+   String Harga;
+   String Model;
+   
+   void MengubahHarga(){
+       System.out.println("Ukuran :"+Ukuran);
+       System.out.println("Merk   :"+Merk);
+       System.out.println("Warna  :"+Warna);
+       System.out.println("Harga  :"+Harga);
+       System.out.println("Model  :"+Model);
+   }
 }
