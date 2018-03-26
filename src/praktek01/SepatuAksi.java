e953fc76ac58fea5dda4bd925008d2c4484d0942
AhmadSyaifuddin <syaifuddinahmad598@gmail.com>
2018-03-26 17:50:49 +0800

    Mengisi Nilai Atribut

--- src/praktek01/SepatuAksi.java
+++ src/praktek01/SepatuAksi.java
@@ -13,7 +13,27 @@ public class SepatuAksi {
     public static void main(String[] args) {
         Sepatu Udin = new Sepatu();
         Sepatu Ali  = new Sepatu();
-        Sepatu Andi = new Sepatu();       
+        Sepatu Andi = new Sepatu();  
+        
+        Udin.Ukuran=42;
+        Udin.Merk="Nike";
+        Udin.Warna="Merah";
+        Udin.Harga="Rp.350.000";
+        Udin.Model="Tali Datar";
+        
+        Ali.Ukuran=44;
+        Ali.Merk="Adidas";
+        Ali.Warna="Biru";
+        Ali.Harga="Rp.460.000";
+        Ali.Model="Tidak Menggunakan Tali";
+        
+        Andi.Ukuran=38;
+        Andi.Merk="Yeezy";
+        Andi.Warna="Hitam";
+        Andi.Harga="Rp.1.500.000";
+        Andi.Model="Tali Zig-Zag";
+        
+      
     }
  
 }
